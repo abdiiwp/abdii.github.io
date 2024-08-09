54346e3a828ea836ed09bcd3eb3a670df6a0efa2
abdiiwp <pandawacoki13@gmail.com>
2024-08-09 23:34:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ADAKAH SERATUS</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark"style="background:#000000;>
+  <!-- Brand/logo -->
+<a class="navbar-brand" href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9Jm_cFjPRhSMxI0SvWKtgb_OSAXqyuVqjLA&usqp=CAU" width="40px" alt=""></a>  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">PINJAM DONG SEIKHLASNYA</a>
+    </li>
+    
+</nav>
+<div class="container">
+  <h2>HEHEHEHE</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+   
+      <label>NAMA:</label>
+      <input type="email" class="form-control" placeholder="KETIK NAMA" NAMA="NIM">
+      <div class="form-group">
+      <label>NOMINAL:</label>
+      <input type="email" class="form-control" placeholder="KETIK NOMINAL" NAMA=NOMINAL>
+     </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
+</body>
+</html>
